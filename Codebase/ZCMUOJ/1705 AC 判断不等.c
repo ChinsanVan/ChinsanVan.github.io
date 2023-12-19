79826c8c4e1c6c2dc493c75e06f3a90b6c1c3d92
Chinsan <147887303+ChinsanVan@users.noreply.github.com>
2023-12-19 11:05:25 +0800

    Create 1705 AC 判断不等.c

---
+++ Codebase/ZCMUOJ/1705 AC 判断不等.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int i,j,m,n,c,k,flag=0;
+	char a[100000];
+	while((scanf("%s",&a))!=EOF){
+		k=strlen(a);flag=1;
+		for(i=1;i<=k;i++){
+			if(k%i==0){
+				flag=1;
+				for(j=i;j<k;j++){
+					if(a[j]!=a[j-i])flag=0;
+				} 
+				//output
+				if(flag==1){
+					printf("%d\n",i);break;
+				}
+			}
+		}
+	}
+	return 0; 
+}
